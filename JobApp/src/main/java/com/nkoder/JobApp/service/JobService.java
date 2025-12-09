65c0d141bcf3a771d97282d821413d709cbb84eb
Nithin-DR <141224153+Nithin-DR@users.noreply.github.com>
2025-12-09 14:20:17 +0530

    Add files via upload

---
+++ JobApp/src/main/java/com/nkoder/JobApp/service/JobService.java
@@ -0,0 +1,31 @@
+package com.nkoder.JobApp.service;
+
+import com.nkoder.JobApp.model.JobPost;
+import com.nkoder.JobApp.repo.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+    @Autowired
+    public JobRepo repo;
+
+
+    // method to add a jobPost
+    public void addJob(JobPost jobPost) {
+        repo.addJob(jobPost);
+
+    }
+
+
+    //method to return all JobPosts
+    public List<JobPost> getAllJobs() {
+        return repo.getAllJobs();
+
+
+    }
+
+
+}
---
+++ JobApp/src/test/java/com/nkoder/JobApp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkoder.JobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
